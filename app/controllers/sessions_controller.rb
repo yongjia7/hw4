2c18872098f435c170df1f95d641abac1c8ed1ba
yongjia7 <yongjia.zhang@kellogg.northwestern.edu>
2022-03-16 05:11:57 +0000

    h

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,32 @@
 class SessionsController < ApplicationController
-  def new
-  end
 
-  def create
+  class SessionsController < ApplicationController
+    def new
+    end
+  
+    def create
+      @user = User.find_by({ email: params["email"] })
+        if @user
+          if BCrypt::Password.new(@user.password) == params["password"]
+            session[:user_id] = @user.id
+            flash[:notice] = "Hello. You are logged in #{@user.username}!"
+            redirect_to "/places"
+          else
+            flash[:notice] = "Incorrect Password"
+            redirect_to "/sessions/new"
+          end
+          
+        else 
+            flash[:notice] = "Not registered. Please register."
+            redirect_to "/sessions/new"
+        end
+  
+    end
+  
+    def destroy
+      session["user_id"] = nil
+      flash[:notice] = "Logged out"
+      redirect_to "/sessions/new"
+    end
   end
-
-  def destroy
-  end
-end
   
